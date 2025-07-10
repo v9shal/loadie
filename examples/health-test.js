92ad47bc4d04f7bba48c9acc3c9deb20bba30377
v9shal <sethivishal145@gmail.com>
2025-07-10 21:02:50 +0530

    necessary changes in health and loadbalancer.js

--- examples/health-test.js
+++ examples/health-test.js
@@ -6,9 +6,7 @@ healthChecker.on('serverHealthChanged', (server, isHealthy) => {
   console.log(`[EVENT] ${server} is now ${isHealthy ? 'healthy' : 'unhealthy'}`);
 });
 
-// Start with one server
 healthChecker.start(['http://localhost:3001']).then(async () => {
-  // Dynamically add another server after 3 seconds
   setTimeout(async () => {
     await healthChecker.addServer('http://localhost:3002');
   }, 3000);
